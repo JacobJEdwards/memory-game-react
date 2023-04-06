f5b0dcada8317b82d1b26e16a659f61254437b49
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-07 00:25:48 +0100

    extracted game component

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
+import type { ReactElement } from 'react'
+import MemoryGame from './MemoryGame'
 import './App.css'
 
-
-type TCell = {
-  number: number,
-  index: number
-}
-
-function App() {
-  const [deck, setDeck] = useState([
-    0, 1,
-    0, 1,
-  ])
-
-  const [flipped, setFlipped] = useState([...deck].map(entry => false))
-
-  const [previousClick, setPreviousClick] = useState<TCell | undefined>(undefined);
-
-  function handleCardClick(index: number) {
-    const clickedNumber = deck[index]
-    const newFlipped = [...flipped];
-    newFlipped[index] = true;
-    setFlipped(newFlipped);
-    
-    
-
-    if (previousClick) {
-      if (previousClick.number !== clickedNumber) {
-
-        setTimeout(() => {
-          newFlipped[index] = false;
-          newFlipped[previousClick.index] = false;
-          setFlipped([...newFlipped])
-        }, 1000)
-      } else {
-        if (flipped.every(value => value)) {
-          alert('you win')
-        }
-      }
-
-      setPreviousClick(undefined);
-    } else {
-      setPreviousClick({
-        number: clickedNumber,
-        index: index
-      });
-    }
-
-
-  }
-
+const App = (): ReactElement => {
   return (
-    <div className="App">
-      <div>
-        <div className="deck">
-          {deck.map((number, index) => (
-            <div className="card" key={index} onClick={() => handleCardClick(index)}>
-              {flipped[index] ? number : " "}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    <main className='App'>
+      <MemoryGame />
+    </main>
   )
 }
-
-export default App
---
+++ src/MemoryGame.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import type { ReactElement } from 'react'
+import './MemoryGame.css'
+
+type TCell = {
+  number: number
+  index: number
+}
+
+const MemoryGame = (): ReactElement => {
+  const [deck, setDeck] = useState([0, 1, 0, 1])
+
+  const [flipped, setFlipped] = useState([...deck].map((entry) => false))
+
+  const [previousClick, setPreviousClick] = useState<TCell | undefined>(
+    undefined
+  )
+
+  function handleCardClick(index: number) {
+    const clickedNumber = deck[index]
+    const newFlipped = [...flipped]
+    newFlipped[index] = true
+    setFlipped(newFlipped)
+
+    if (previousClick) {
+      if (previousClick.number !== clickedNumber) {
+        setTimeout(() => {
+          newFlipped[index] = false
+          newFlipped[previousClick.index] = false
+          setFlipped([...newFlipped])
+        }, 1000)
+      } else {
+        if (flipped.every((value) => value)) {
+          alert('you win')
+        }
+      }
+
+      setPreviousClick(undefined)
+    } else {
+      setPreviousClick({
+        number: clickedNumber,
+        index: index,
+      })
+    }
+  }
+
+  return (
+    <section className='deck'>
+      {deck.map((number, index) => (
+        <div
+          className='card'
+          key={index}
+          onClick={() => handleCardClick(index)}>
+          {flipped[index] ? number : ' '}
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export default MemoryGame
