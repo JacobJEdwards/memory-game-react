cffd5ca25f06769a188ff654c15ce5f699805a4a
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-07 11:29:04 +0100

    refactoring and reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import './MemoryGame.css'
+import React from 'react'
+import MemoryGame from './MemoryGame'
+import './App.css'
 
-const NUMBER_OF_CARDS = 4
-const CARDS: number[] = [0, 1, 0, 1]
-
-function shuffle<T>(array: T[]): T[] {
-  const shuffledArray = [...array]
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
-  }
-  return shuffledArray
-}
-
-function MemoryGame(): JSX.Element {
-  const [deck, setDeck] = useState<number[]>(shuffle(CARDS))
-  const [flipped, setFlipped] = useState<boolean[]>(
-    Array(NUMBER_OF_CARDS).fill(false)
-  )
-  const [matched, setMatched] = useState<number[]>([])
-  const [disabled, setDisabled] = useState<boolean>(false)
-
-  useEffect(() => {
-    if (flipped.every((value) => value)) {
-      setDisabled(true)
-      setTimeout(() => {
-        setFlipped(Array(NUMBER_OF_CARDS).fill(false))
-        setMatched([])
-        setDisabled(false)
-      }, 1000)
-    }
-  }, [flipped, matched])
-
-  function handleCardClick(index: number): void {
-    if (disabled || flipped[index] || matched.includes(index)) {
-      return
-    }
-
-    const newFlipped = [...flipped]
-    newFlipped[index] = true
-    setFlipped(newFlipped)
-
-    const flippedCards = newFlipped.reduce<number[]>((acc, val, idx) => {
-      if (val) {
-        acc.push(idx)
-      }
-      return acc
-    }, [])
-
-    if (flippedCards.length === 2) {
-      setDisabled(true)
-      const [firstIndex, secondIndex] = flippedCards
-      const [firstCard, secondCard] = [deck[firstIndex], deck[secondIndex]]
-
-      if (firstCard === secondCard) {
-        setMatched([...matched, firstIndex, secondIndex])
-        setDisabled(false)
-      } else {
-        setTimeout(() => {
-          newFlipped[firstIndex] = false
-          newFlipped[secondIndex] = false
-          setFlipped(newFlipped)
-          setDisabled(false)
-        }, 1000)
-      }
-    }
-  }
-
+const App = () => {
   return (
-    <section className='deck'>
-      {deck.map((number, index) => (
-        <div
-          className='card'
-          key={index}
-          onClick={() => handleCardClick(index)}>
-          {flipped[index] || matched.includes(index) ? number : ' '}
-        </div>
-      ))}
-    </section>
+    <div className='App'>
+      <h1>Memory Game</h1>
+      <MemoryGame />
+    </div>
   )
 }
 
-export default MemoryGame
+export default App
--- src/MemoryGame.tsx
+++ src/MemoryGame.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback, memo } from 'react'
 import './MemoryGame.css'
 
-const NUMBER_OF_CARDS = 4
-const CARDS = [0, 1, 0, 1]
+const CARDS: number[] = [0, 1, 0, 1]
 
-function shuffle(array: number[]) {
+type GameState = {
+  deck: number[]
+  flipped: boolean[]
+  matched: number[]
+  disabled: boolean
+  gameOver: boolean
+}
+
+function shuffle<T>(array: T[]): T[] {
   const shuffledArray = [...array]
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
@@ -13,56 +20,136 @@ function shuffle(array: number[]) {
   return shuffledArray
 }
 
-function MemoryGame() {
-  const [deck, setDeck] = useState(shuffle(CARDS))
-  const [flipped, setFlipped] = useState(Array(NUMBER_OF_CARDS).fill(false))
-  const [matched, setMatched] = useState<number[]>([])
+const Card = memo(
+  (props: {
+    value: number
+    flipped: boolean
+    matched: boolean
+    onClick: () => void
+  }): JSX.Element => {
+    const { value, flipped, matched, onClick } = props
+    const isflipped = flipped || matched
 
-  function handleCardClick(index: number) {
-    if (flipped[index] || matched.includes(index)) {
-      return
+    return (
+      <div
+        className={`card ${isflipped ? 'flipped' : ''}`}
+        onClick={onClick}>
+        {flipped || matched ? value : ' '}
+      </div>
+    )
+  }
+)
+
+const MemoryGame = (): JSX.Element => {
+  const [state, setState] = useState<GameState>({
+    deck: shuffle(CARDS),
+    flipped: Array(CARDS.length).fill(false),
+    matched: [],
+    disabled: false,
+    gameOver: false,
+  })
+
+  // win condition
+  useEffect(() => {
+    if (state.flipped.every((value) => value)) {
+      setState((prevState) => ({
+        ...prevState,
+        disabled: true,
+        gameOver: true,
+      }))
     }
+  }, [state.flipped])
 
-    const newFlipped = [...flipped]
-    newFlipped[index] = true
-    setFlipped(newFlipped)
-
-    const flippedCards = newFlipped.reduce((acc, val, idx) => {
-      if (val) {
-        acc.push(idx)
+  const handleCardClick = useCallback(
+    (index: number): void => {
+      if (
+        state.disabled ||
+        state.flipped[index] ||
+        state.matched.includes(index)
+      ) {
+        return
       }
-      return acc
-    }, [])
 
-    if (flippedCards.length === 2) {
-      const [firstIndex, secondIndex] = flippedCards
-      const [firstCard, secondCard] = [deck[firstIndex], deck[secondIndex]]
+      const newFlipped = [...state.flipped]
+      newFlipped[index] = true
 
-      if (firstCard === secondCard) {
-        setMatched([...matched, firstIndex, secondIndex])
-        if (matched.length + 2 === NUMBER_OF_CARDS) {
-          alert('You win!')
+      const flippedCards = newFlipped.reduce<number[]>((acc, val, idx) => {
+        if (val) {
+          acc.push(idx)
+        }
+        return acc
+      }, [])
+
+      if (flippedCards.length === 2) {
+        setState((prevState) => ({
+          ...prevState,
+          flipped: newFlipped,
+          disabled: true,
+        }))
+
+        const [firstIndex, secondIndex] = flippedCards
+        const [firstCard, secondCard] = [
+          state.deck[firstIndex],
+          state.deck[secondIndex],
+        ]
+
+        if (firstCard === secondCard) {
+          setState((prevState) => ({
+            ...prevState,
+            matched: [...prevState.matched, firstIndex, secondIndex],
+            disabled: false,
+          }))
+        } else {
+          setTimeout(() => {
+            newFlipped[firstIndex] = false
+            newFlipped[secondIndex] = false
+            setState((prevState) => ({
+              ...prevState,
+              flipped: newFlipped,
+              disabled: false,
+            }))
+          }, 1000)
         }
       } else {
-        setTimeout(() => {
-          newFlipped[firstIndex] = false
-          newFlipped[secondIndex] = false
-          setFlipped(newFlipped)
-        }, 1000)
+        setState((prevState) => ({
+          ...prevState,
+          flipped: newFlipped,
+        }))
       }
-    }
+    },
+    [state.deck, state.disabled, state.flipped, state.matched]
+  )
+
+  const resetGame = (): void => {
+    setState((prevState) => ({
+      ...prevState,
+      deck: shuffle(prevState.deck),
+      flipped: Array(CARDS.length).fill(false),
+      matched: [],
+      disabled: false,
+      gameOver: false,
+    }))
   }
 
   return (
-    <section className='deck'>
-      {deck.map((number, index) => (
-        <div
-          className='card'
-          key={index}
-          onClick={() => handleCardClick(index)}>
-          {flipped[index] || matched.includes(index) ? number : ' '}
+    <section>
+      <section className='deck'>
+        {state.deck.map((number, index) => (
+          <Card
+            key={index}
+            value={number}
+            flipped={state.flipped[index]}
+            matched={state.matched.includes(index)}
+            onClick={() => handleCardClick(index)}
+          />
+        ))}
+      </section>
+      {state.gameOver && (
+        <div>
+          <h1>You Won!</h1>
+          <button onClick={resetGame}>Restart</button>
         </div>
-      ))}
+      )}
     </section>
   )
 }
