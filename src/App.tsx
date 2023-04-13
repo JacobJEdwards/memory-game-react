99f785301c4457abb7f96bf2754cfad3e1dbb74b
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-13 21:22:58 +0100

    semantic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
-import MemoryGame from './MemoryGame'
-import StartScreen from './StartScreen'
-import Countdown from './Countdown'
-import './App.css'
+import React, { useState } from "react";
+import MemoryGame from "./MemoryGame";
+import StartScreen from "./StartScreen";
+import Countdown from "./Countdown";
+import "./App.css";
 
 const App = () => {
-  const [gameStarted, setGameStarted] = useState(false)
-  const [countStarted, setCountStarted] = useState(false)
+    const [gameStarted, setGameStarted] = useState(false);
+    const [countStarted, setCountStarted] = useState(false);
 
-  const [countDown, setCountDown] = useState(3)
+    const [countDown, setCountDown] = useState(3);
 
-  return (
-    <div className='App'>
-      <h1>Memory Game</h1>
-      {gameStarted && <MemoryGame numPairs={3} />}
+    return (
+        <main className="App">
+            <h1>Memory Game</h1>
+            {gameStarted && <MemoryGame numPairs={3} />}
 
-      {!gameStarted && !countStarted && (
-        <StartScreen isStart={() => setCountStarted(true)} />
-      )}
+            {!gameStarted && !countStarted && (
+                <StartScreen isStart={() => setCountStarted(true)} />
+            )}
 
-      {!gameStarted && countStarted && (
-        <Countdown
-          countDown={countDown}
-          countFinished={() => setGameStarted(true)}
-        />
-      )}
-    </div>
-  )
-}
+            {!gameStarted && countStarted && (
+                <Countdown
+                    countDown={countDown}
+                    countFinished={() => setGameStarted(true)}
+                />
+            )}
+        </main>
+    );
+};
 
-export default App
+export default App;
