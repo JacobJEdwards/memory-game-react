36ad6a6fd927c589194e74b47bd605715dcad82b
JacobJEdwards <jacob.john1@talktalk.net>
2023-04-13 19:58:17 +0100

    see previous

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/memory-game-react/',
-  plugins: [react()],
-})
+    base: "/memory-game-react/",
+    plugins: [react(), ghPages()],
+});
